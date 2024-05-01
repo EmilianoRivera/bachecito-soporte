4858f932ba92f685c0da9546fe2c3bccea905902
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-05-01 02:24:35 -0600

    cambios en tickets

--- src/app/Cuenta/Desarrolladores/Tickets/page.jsx
+++ src/app/Cuenta/Desarrolladores/Tickets/page.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Tickets() {
+  useEffect(()=> {
+    async function fetchData () {
+      try {
+        
+      } catch (error) {
+        console.error("ERROR: ", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div>Tickets</div>
   )
