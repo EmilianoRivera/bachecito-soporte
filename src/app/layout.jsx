2754c48ef78753c98d9876b08874a2e5b17a8110
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-06-06 01:41:37 -0600

    diosito

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import "./globals.css";
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 
 /*
 export const metadata = {
